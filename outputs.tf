7c6710465b96000f83ee2c43f8ebafb144707c76
yosrijendoubi <60221774+yosrijendoubi@users.noreply.github.com>
2022-02-24 10:59:04 +0100

    Adding test

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.resource_group.name
+  value = azurerm_resource_group.pipelinerg.name
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.storage_account.name
+  value = azurerm_storage_account.pipelinesa.name
 }
 
 output "storage_container_name" {
-  value = azurerm_storage_container.container.name
+  value = azurerm_storage_container.pipelinecontainer2.name
 }
